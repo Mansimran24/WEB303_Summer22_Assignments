a693075153ef2d71e22ea33048988fb5b2864835
Mansimran Kaur <91445014+Mansimran24@users.noreply.github.com>
2022-07-26 06:30:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 $(function() {
 
-    var $tbody = $('tbody'); // reference <tbody> element on the page
-    var $search = $('#search'); // reference to the search input box
+    var $tbody = $('tbody'); 
+    var $search = $('#search');
     var cache = [];
-    var characterCount = [0, 0]; //
-    var $buttons = $('#buttons'); // Store buttons
+    var characterCount = [0, 0]; 
+    var $buttons = $('#buttons'); 
 
 
     $.getJSON("kapilshow.json").done((data) => {
-        // jQuery.each of the players in the array
+
         $.each(data.kapilshow, function(key, val) {
             console.log("data key: ", key, " and data value: ", val);
 
-            var $row = $('<tr></tr>'); // Create their row
-            // populate data
+            var $row = $('<tr></tr>'); 
             $row.append($('<td></td>').text(val.name));
             $row.append($('<td></td>').text(val.character));
             $row.append($('<td></td>').text(val.age));
@@ -21,53 +20,46 @@ $(function() {
 
             $tbody.append($row);
 
-            cache.push({ // Create the cache that contains several values
-                element: $row, // Reference to the row element
-                // The text we're searching against (which in this case is first name)
+            cache.push({ 
+                element: $row, 
                 fname: val.name.trim().toLowerCase(),
-                // we only need the first character of the last name for filtering
                 ncharacter: val.character.trim().toLowerCase().charAt(0)
             });
 
-            // add to the count for last names starting with a - m, and n - z
             if ("a" <= val.character.trim().toLowerCase().charAt(0) && "m" >= val.character.trim().toLowerCase().charAt(0)) {
-                characterCount[0]++; // index 0 will be for if the last name starts with a - m
+                characterCount[0]++; 
             } else { // n - z
-                characterCount[1]++; // index 1 will be for if the last name starts with n - z
+                characterCount[1]++; 
             }
         });
 
-        // after we process each player, we want to add the buttons to the page.
-        // We need to build the buttons after the data comes back from the server
-        // otherwise characterCount at both indexes will alawys be 0 on the page
-        $('<button/>', { // Create button
-            text: 'A - M (' + characterCount[0] + ')', // Add text, and the count for occurances
-            click: function() { // Add click handler
-                $(this) // Get clicked button
-                    .addClass('active') // Make it active
-                    .siblings() // Get its siblings
-                    .removeClass('active'); // Remove active class
-                cache.forEach((characters) => { // Each cache entry
-                    // check if character is in range
+        
+        $('<button/>', { 
+            text: 'A - M (' + characterCount[0] + ')', 
+            click: function() {
+                $(this) 
+                    .addClass('active') 
+                    .siblings() 
+                    .removeClass('active'); 
+                cache.forEach((characters) => { 
                     if ("a" <= characters.ncharacter && "m" >= characters.ncharacter) {
                         characters.element.show();
-                    } else { // not in range, hide this chess player
+                    } else { 
                         characters.element.hide();
                     }
                 });
 
             }
-        }).appendTo($buttons); // Add to buttons
+        }).appendTo($buttons); 
 
-        $('<button/>', { // Create button
-            text: `N - Z (${characterCount[1]})`, // Add text
-            click: function() { // Add click handler
-                $(this) // Get clicked button
-                    .addClass('active') // Make it active
-                    .siblings() // Get its siblings
-                    .removeClass('active'); // Remove active class
-                cache.forEach((characters) => { // Each cache entry
-                    // check if character is in range
+        $('<button/>', { 
+            text: `N - Z (${characterCount[1]})`, 
+            click: function() { 
+                $(this) 
+                    .addClass('active') 
+                    .siblings()
+                    .removeClass('active');
+                cache.forEach((characters) => { 
                     if ("n" <= characters.ncharacter && "z" >= characters.ncharacter) {
                         characters.element.show();
                     } else {
@@ -75,38 +67,31 @@ $(function() {
                     }
                 });
             }
-        }).appendTo($buttons); // Add to buttons
+        }).appendTo($buttons); 
 
     });
 
 
-
-    // method operates on the search input, so the keyword this references the input#filter-search element
     function filter() {
-        var query = this.value.trim().toLowerCase(); // Get query
-        if (query) { // If there’s a query
-            cache.forEach(function(characters) { // Each cache entry
-                var index = 0; // Set index to 0
-                index = characters.fname.indexOf(query); // Is text in there?
-                if (index != -1) { // we found the string in their first name
-                    characters.element.addClass("active"); // we will apply colours based on this class
-                } else { // player first name doesn't have the query string, make sure it's not higlighted
+        var query = this.value.trim().toLowerCase(); 
+        if (query) { 
+            cache.forEach(function(characters) { 
+                var index = 0; 
+                index = characters.fname.indexOf(query); 
+                if (index != -1) { 
+                    characters.element.addClass("active"); 
+                } else { 
                     characters.element.removeClass("active")
                 }
             });
-        } else { // if the search is empty, nobody should be highlighted
+        } else { 
             $('tbody tr').removeClass("active");
         }
     }
-    // if the search input box supports the input event, we want to use it instead of the keyup event
     if ('oninput' in $search[0]) {
-        // Use input event to call filter()
         $search.on('input', filter);
-    } else { // Otherwise
-        // Use keyup event to call filter()
+    } else { 
         $search.on('keyup', filter);
     }
-
-
 
 });
