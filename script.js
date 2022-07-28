ac71a8c8fb3f015e4637eff574322b25be9328fb
Mansimran Kaur <91445014+Mansimran24@users.noreply.github.com>
2022-07-27 21:12:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -50,50 +50,7 @@ $(function() {
         $search.on('keyup', filter);
     }
   
-    var compare = { // Declare object
-        name: function(a, b) { // Add name() method
-        a = a.replace(/^the /i, ''); // Remove The
-        b = b.replace(/^the /i, ''); // Remove The
-        if (a < b) { // If a less than b
-        return -1; // Return -1
-        } else { // Otherwise
-        // If a greater than b return 1 otherwise return 0
-        return a > b ? 1 : 0; 
-        }
-        }, // More methods go here...
-        date: function(a, b) { // Add a method called date
-            a = new Date(a); // New object to hold date
-            b = new Date(b); // New object to hold date
-            return a - b; // Return a minus b
-            },
-
-            $('.sortable').each(function() {
-                var $table = $(this); // This table
-                var $tbody = $table.find('tbody'); // Table body
-                var $controls = $table.find('th'); // Table headers
-                var rows = $tbody.find('tr').toArray();
-                 // Array of rows
-                $controls.on('click', function() { // Event handler
-                var $header = $(this); // Get header
-                var order = $header.data('sort'); // Get data type
-                var column;     
-                });
-            }),
-            if ($header.is('.ascending') || $header.is('.descending'))
-            { 
-                // Toggle to other class
-                $header.toggleClass('ascending descending'); 
-                // Reverse the array
-                $tbody.append(rows.reverse()); 
-            } 
-            else {
-
-            }
-
-                        
-        }
-      
-                                          
+                                     
 
 
 });
