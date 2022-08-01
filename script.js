745acbb744d8409531f17024f93bff7c7d0d045c
Mansimran Kaur <91445014+Mansimran24@users.noreply.github.com>
2022-07-31 20:18:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -57,99 +57,51 @@ $(function() {
 
 
 
-var names = $('#name')
-var $search = $('#search'); // Get input
-var cache = [];
-var $tableBody=$('tbody')
-$.each(characters, function(key,val) { 
-
-// Store current charcater
-var $row = $('<tr></tr>'); // Create row for them
-$row.append($('<td></td>').text(val.name)); // Add name
-$row.append($('<td></td>').text(val.description)); // Add desvription
-$row.append($('<td></td>').text(val.age));
-$row.append($('<td></td>').text(val.dob));
-$tableBody.append($row);
-
-// Create array
-// Each img
-    cache.push({ // Add to cache
-        element: $row, // This image
-        name: val.name.trim().toLowerCase() // Its alt text
+    var compare = { 
+        name: function(a, b) { 
+            a = a.replace(/^the /i, ''); 
+        b = b.replace(/^the /i, ''); 
+        if (a < b) { 
+            return -1; 
+        } else { 
+        return a > b ? 1 : 0;
+        }
+        },
+        date: function(a, b) { 
+            a = new Date(a);
+            b = new Date(b); 
+            return a - b; 
+        }
+        }
+        $('.sortable').each(function() {
+            var $table = $(this); 
+            var $tbody = $table.find('tbody'); 
+            var $controls = $table.find('th'); 
+            var rows = $tbody.find('tr').toArray(); 
+            $controls.on('click', function() { 
+                var $header = $(this); 
+            
+        var order = $header.data('sort'); 
+        if ($header.is('.ascending') || $header.is('.descending')) { 
+            $header.toggleClass('ascending descending');
+            
+            $tbody.append(rows.reverse());
+            } else {
+                $header.addClass('ascending'); 
+    $header.siblings().removeClass('ascending descending'); 
+    if (compare.hasOwnProperty(order)) {
+    column = $controls.index(this); 
+    rows.sort(function(a, b) { 
+        a = $(a).find('td').eq(column).text();
+        b = $(b).find('td').eq(column).text();
+        return compare[order](a, b); 
     });
-});
-function filter() {
-    var query = this.value.trim().toLowerCase(); // Get query
-    if (query) { // If there’s a query
-        cache.forEach(function(characters) { // Each cache entry
-            var index = 0; // Set index to 0
-            index = characters.name.indexOf(query); // Is text in there?
-            if (index != -1) { // we found the string in their first name
-                characters.element.addClass("active"); // we will apply colours based on this class
-            } else { // player first name doesn't have the query string, make sure it's not higlighted
-                characters.element.removeClass("active")
-            }
-        });
-    } else { // if the search is empty, nobody should be highlighted
-        $('tbody tr').removeClass("active");
+    $tbody.append(rows);}
     }
-}
-// if the search input box supports the input event, we want to use it instead of the keyup event
-if ('oninput' in $search[0]) {
-    // Use input event to call filter()
-    $search.on('input', filter);
-} else { // Otherwise
-    // Use keyup event to call filter()
-    $search.on('keyup', filter);
-}//Sorting
-var compare = { // Declare object
-name: function(a, b) { // Add name() method
-    a = a.replace(/^the /i, ''); // Remove The
-b = b.replace(/^the /i, ''); // Remove The
-if (a < b) { // If a less than b
-    return -1; // Return -1
-} else { // Otherwise
-// If a greater than b return 1 otherwise return 0
-return a > b ? 1 : 0;
-}
-},
-date: function(a, b) { // Add a method called date
-    a = new Date(a); // New object to hold date
-    b = new Date(b); // New object to hold date
-    return a - b; // Return a minus b
-}
-}
-$('.sortable').each(function() {
-    var $table = $(this); // This table
-    var $tbody = $table.find('tbody'); // Table body
-    var $controls = $table.find('th'); // Table headers
-    var rows = $tbody.find('tr').toArray(); // Array of rows
-    $controls.on('click', function() { // Event handler
-        var $header = $(this); // Get header
+            });
+    })
     
-var order = $header.data('sort'); // Get data typevar column; 
-if ($header.is('.ascending') || $header.is('.descending')) { // Toggle to other class
-    $header.toggleClass('ascending descending');
-    // Reverse the array
-    $tbody.append(rows.reverse());
-    } else {
-        $header.addClass('ascending'); // Add class to header// Remove asc or desc from all other headers
-$header.siblings().removeClass('ascending descending'); // If compare object has method of that name
-if (compare.hasOwnProperty(order)) {
-column = $controls.index(this); // Column’s index no
-rows.sort(function(a, b) { // Call sort() on rows
-a = $(a).find('td').eq(column).text();// Text of column row a
-b = $(b).find('td').eq(column).text();// Text of column row b
-return compare[order](a, b); // Call compare method
-});
-$tbody.append(rows);}
-}
-    });
-})
-                          
+                                          
 
 
 });
-
-    
-    
